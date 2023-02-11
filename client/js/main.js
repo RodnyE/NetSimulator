0cac09864d9304067917e72893f0a7bf9ef6c973
Rodny Estrada <rrodnyestrada1@gmail.com>
2023-02-11 00:43:14 -0500

    added landscape force

--- client/js/main.js
+++ client/js/main.js
@@ -1,16 +1,32 @@
 /**
- * primary script
+ * Primary script
  */
 
 function main () {
-  
-  // browser console
-  if (typeof eruda !== "undefined") {
-    eruda.init();
-    eruda.show();
-    console.log("Eruda v" + eruda.version);
-  }
-  
 
-  main = null;
+    // browser console
+    if (typeof eruda !== "undefined") {
+        eruda.init();
+        eruda.show();
+        console.log("Eruda v" + eruda.version);
+    }
+
+    // landscape forcer
+    forceLandscape.start();
+    forceLandscape.onShow(onPortrait);
+    forceLandscape.onHide(onLandscape);
+
+
+    main = null;
+}
+
+
+// Event: Orientation change to portrait
+function onPortrait () {
+    console.log("orientation:portrait");
+}
+
+// Event: Orientation change to landscape
+function onLandscape () {
+    console.log("orientation:landscape");
 }
